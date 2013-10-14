bb086e19e24366c9b66855cc9aa93eced32bce93
huiqing <hl@kent.ac.uk>
2013-10-14 12:47:59 +0100

    layout change.

--- src/devo_app.erl
+++ src/devo_app.erl
@@ -8,16 +8,17 @@
 
 %% API.
 start(_Type, _Args) ->
-	Dispatch = cowboy_router:compile([
-                                          {'_', [
-                       {"/", devo_toppage_handler, []},
-                       {"/websocket", devo_ws_handler, []},
-                       {"/static/[...]", cowboy_static, [
-                                                          {directory, {priv_dir, devo, [<<"static">>]}},
-				{mimetypes, {fun mimetypes:path_to_mimes/2, default}}
-                                                         ]}
-                      ]}
-                                         ]),
+    Dispatch = 
+        cowboy_router:compile([
+                               {'_', [
+                                      {"/", devo_toppage_handler, []},
+                                      {"/websocket", devo_ws_handler, []},
+                                      {"/static/[...]", cowboy_static, [
+                                                                        {directory, {priv_dir, devo, [<<"static">>]}},
+                                                                        {mimetypes, {fun mimetypes:path_to_mimes/2, default}}
+                                                                       ]}
+                                     ]}
+                              ]),
     {ok, _} = cowboy:start_http(http, 100, [{port, 8080}],
                                     [{env, [{dispatch, Dispatch}]}]),
     devo_sup:start_link().
