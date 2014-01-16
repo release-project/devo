5188ad0e854660367ddd804f3a82dd850c827955
huiqing <hl@kent.ac.uk>
2014-01-16 23:56:09 +0000

    updated

--- tests/orbit.erl
+++ tests/orbit.erl
@@ -2,7 +2,11 @@
 
 -compile(export_all).
 
-run() ->
+run_on_one_node() ->
+    Nodes=['node1@127.0.0.1'],
+    bench:dist_seq(fun bench:g124/1, 10000, 8,Nodes).
+   
+run_on_multi_nodes() ->
     Nodes = ['node1@127.0.0.1', 'node2@127.0.0.1', 'node3@127.0.0.1',
              'node4@127.0.0.1', 'node5@127.0.0.1'],
     bench:dist_seq(fun bench:g124/1, 10000, 8,Nodes).
