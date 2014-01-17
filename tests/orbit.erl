fc2d689e2ca41b9529c54de30eff10d2ae5ff9a1
huiqing <hl@kent.ac.uk>
2014-01-17 12:23:33 +0000

    updated.

--- tests/orbit.erl
+++ tests/orbit.erl
@@ -4,10 +4,19 @@
 
 run_on_one_node() ->
     Nodes=['node1@127.0.0.1'],
-    bench:dist_seq(fun bench:g124/1, 10000, 8,Nodes).
+    bench:dist_seq(fun bench:g124/1, 100000, 8,Nodes).
    
-run_on_multi_nodes() ->
+run_on_five_nodes() ->
     Nodes = ['node1@127.0.0.1', 'node2@127.0.0.1', 'node3@127.0.0.1',
              'node4@127.0.0.1', 'node5@127.0.0.1'],
-    bench:dist_seq(fun bench:g124/1, 10000, 8,Nodes).
-    
+    bench:dist_seq(fun bench:g124/1, 100000, 8,Nodes).
+      
+teardown(N) -> 
+    F=fun(I) ->
+              Node=list_to_atom("node"++integer_to_list(I)++"@127.0.0.1"),
+              rpc:call(Node, erlang, halt, [])
+      end,
+    lists:foreach(fun(I) -> F(I) end, lists:seq(1, N)).
+
+stop(Node)->
+    rpc:call(Node, erlang, halt, []).
--- tests/start.sh
+++ tests/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-erl -name $1  -setcookie "secret" -pa ../ebin -config s_group.config 
+erl -name $1 $2 -setcookie "secret" -pa ../ebin -config s_group.config 
 
  
