07efb5876ae0be2f9dd60cfea55ee5076f957879
huiqing <hl@kent.ac.uk>
2014-01-23 22:30:12 +0000

    removed duplicated output.

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-erl -pa ebin deps/*/ebin -s devo \
-    -eval "io:format(\"Point your browser at http://localhost:8080/ to use a simple websocket client~n\")."
+erl -setcookie "secret" -pa ebin deps/*/ebin -s devo 
 
  
